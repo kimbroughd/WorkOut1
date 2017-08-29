3e20d2d96ee67d9885de64b882bc223ebc578789
kimbroughd <31328710+kimbroughd@users.noreply.github.com>
2017-08-29 11:59:22 -0400

    Add files via upload

---
+++ solution/PlaceHolder.java
@@ -0,0 +1,9 @@
+package solution;
+
+public class PlaceHolder
+{
+    Award aw = new Award();
+    DisplayTabular dT = new DisplayTabular();
+    System.out.print(dT.display(aw.summaCumLaude, 
+        aw.magnaCumLaude, aw.cumLaude));
+}
